26a29eaa949130233196a1c2c32e186803a38d55
Innocent Mazando <innocent.mazando@falcorp.co.za>
2022-10-10 16:39:21 +0200

    added panic lists

--- src/pages/Panics/ListsPanics.js
+++ src/pages/Panics/ListsPanics.js
@@ -1,7 +1,32 @@
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const config = {
+  method: "get",
+  url: "http://192.168.8.158:8000/api/panics",
+  headers: {
+    Accept: "application/json",
+    Authorization: "Bearer " + localStorage.getItem("accessToken"),
+  },
+};
 
 const ListsPanics = () => {
-  return <div>kkkkk</div>;
+  const [panics, setPanics] = useState([]);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    axios(config)
+      .then(function (response) {
+        console.log(response);
+        setPanics(response.data.data.panics);
+      })
+      .catch((err) => {
+        setError(err);
+        console.log(err.message);
+      });
+  }, []);
+
+  return <h2>silly us</h2>;
 };
 
 export default ListsPanics;
